7c9ac3b00f283c301c9ce2423a8d23d9a7ffa24d
ThuBoo <tn6642455@gmail.com>
2024-10-06 23:34:21 +0700

    Add files via upload

---
+++ THLTHDT_CAU2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package thlthdt_cau2;
+public class THLTHDT_CAU2 {
+
+   /**
+    * @param args the command line arguments
+    */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String ten = "Nguyen Thi Thu";
+        int tuoi = 19;
+        double chieucao= 1.60;
+        boolean thichlaptrinh= true;
+        System.out.println(" ten " + ten);
+        System.out.println(" tuoi " + tuoi);
+        System.out.println(" chieucao " + chieucao);
+        System.out.println(" thichlaptrinh" + thichlaptrinh);
+    }
+    
+}
