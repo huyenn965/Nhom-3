237eb1437091b8a7b06d1a0b87f9b256f6f2fde4
TuanAnhNguyen-2005 <Tn271574@gmail.com>
2024-10-07 00:13:10 +0700

    Add files via upload

---
+++ Tuan2_Cau1.java
@@ -0,0 +1,8 @@
+public class Baitap {
+    public static void main(String[] args) {
+        String thongbao = "tuan";
+        System.out.println("thong bao: "+ thongbao);
+        final int x = 100;
+        System.out.println("x:"+ x);
+    }
+}
