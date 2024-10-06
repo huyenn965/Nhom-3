c7d6587af682c3b2b17c7f806762bbc3e68ec4c7
ThaiThanhTu0212 <thanhtu98912@gmail.com>
2024-10-07 00:03:04 +0700

    Add files via upload

---
+++ Cau1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Cau1 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("nhap ho ten: ");
+        String ten = scanner.nextLine();
+        System.out.println(" hello " + ten);
+        scanner.close();
+    }
+}
