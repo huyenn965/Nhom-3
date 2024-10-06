03364169531279758261786a94184ba8d74dcb73
TuanAnhNguyen-2005 <Tn271574@gmail.com>
2024-10-07 00:13:29 +0700

    Add files via upload

---
+++ Tuan2_Cau2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Cau_2 {
+    public static void main(String[] args) {
+        String ten = "Nguyen Anh Tuan";
+        int tuoi = 19;
+        double chieucao = 1.72;
+        boolean laptrinh = true;
+        System.out.println("ten: "+ ten);
+        System.out.println("tuoi: "+ tuoi);
+        System.out.println("chieu cao: "+ chieucao);
+        System.out.println("co thich lap trinh khong (true/false): "+ laptrinh);
+    }
+}
